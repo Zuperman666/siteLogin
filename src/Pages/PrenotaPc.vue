a177de4bc4fe32626726abfa3df0f03f12bcfe8d
manuel <manuel.fia@synclab.net>
2020-12-11 21:45:33 +0100

    eventi + prenota pc

--- src/Pages/PrenotaPc.vue
+++ src/Pages/PrenotaPc.vue
@@ -1,23 +1,102 @@
 <template>
-    <div>
-        <HeaderContainer/>
-        <div class="fotoBackground"></div>
-        <FormPrenotazione />
+    <div class="bg-white">
+        <div class="fixedHeaderContainer">
+            <HeaderContainer/>
+        </div>
+        <div class="container text-left">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <div class="H1">
+                        Contatti
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <form>
+                        <div class="row form-group">
+                            <div class="col">
+                                <label for="name">Nome</label>
+                                <input id="name" type="text" class="form-control" v-model="dataSave.nome"/>
+                            </div>
+                            <div class="col">
+                                <label for="lastName">Cognome</label>
+                                <input id="lastName" type="text" class="form-control" v-model="dataSave.cognome"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail">Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail" v-model="dataSave.email"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="type">Tipo Postazione</label>
+                            <select id="type" class="form-control" v-model="dataSave.typePc"
+                                    onchange="dataSave.typePc = target.value">
+                                <option>Pc</option>
+                                <option>Racing</option>
+                            </select>
+                        </div>
+                        <div class="form-group" v-if="dataSave.typePc === 'Pc'">
+                            <label for="topicSelect">Numero Persone</label>
+                            <select id="topicSelect" class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                            </select>
+                        </div>
+                        <div class="form-group" v-if="dataSave.typePc === 'Racing'">
+                            <label for="numberRacing">Numero Persone</label>
+                            <select id="numberRacing" class="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                            </select>
+                        </div>
+                        <div class="form-group form-check d-flex">
+                            <label class="form-check-label" for="exampleCheck"
+                            >Data Processing</label
+                            >
+                            <input
+                                    type="checkbox"
+                                    class="form-check-input"
+                                    id="exampleCheck"
+                                    v-model="dataSave.check"
+                            />
+                        </div>
+                        <div class="btn btn-primary" v-on:click="sendData(dataSave)">Invia</div>
+                    </form>
+                </div>
+            </div>
+        </div>
         <FooterElement/>
     </div>
 </template>
 <script>
-    import FooterElement from '../components/Footer.vue';
-    import FormPrenotazione from "../components/FormPrenotazione";
+    import FooterElement from "../components/Footer.vue";
+
     export default {
-        name: 'PrenotaPc',
-        components:{FooterElement,FormPrenotazione},
-    }
+        name: "Contattaci",
+        props: ['typePc'],
+        components: {FooterElement},
+        data() {
+            return {dataSave: {nome: "", cognome: "", email: "", check: false, typePc: this.typePc}};
+        },
+        methods: {
+            sendData: (dataSave) => {
+                console.log(dataSave);
+            }
+        }
+    };
+
 </script>
 <style lang="scss" scoped>
-.fotoBackground{
-    width:100%;
-    height: 500px;
-    background: grey;
-}
+    .fixedHeaderContainer {
+        background-image: url("../assets/MNT00276.png");
+        background-size: cover;
+        min-height: 80vh;
+    }
 </style>
