2e7483273e5947af0f7baa35c9a1f0e21891cc2c
manuel <manuel.fia@synclab.net>
2021-01-21 20:10:39 +0100

    aggiunto seo

--- src/Pages/404.vue
+++ src/Pages/404.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-          FILE NOT FOUND
+          <h1>
+              FILE NOT FOUND
+          </h1>
     </div>
 </template>
 <script>
     export default {
         name: "404",
+        metaInfo: {
+            title: '404 LoginGaming',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'description', content: 'Pagina per la gestione degli errori di pagina non trovata del LoginGaming' },
+                { name:'keyword',content:'Page Not Found LoginGaming'}
+            ]
+        },
         mounted: function () {
             window.scrollTo({ top: 0, behavior: "smooth" });
         },
